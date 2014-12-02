429c9db7ec444aa5ffb4b6c6eb3664256a6624a2
Simon Rodriguez <kosua20@gmail.com>
2014-12-02 11:08:07 +0100

    New MainTests class + createDisk method

--- src/vfsCore/Core.java
+++ src/vfsCore/Core.java
@@ -1,6 +1,47 @@
 package vfsCore;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+
 public class Core {
 	private Hierarchy fullHierarchy;
 	private Hierarchy currentNode;
+	
+	
+	public boolean createDisk(String diskPath, int i) {
+		
+		try (RandomAccessFile randomAccessFile = new RandomAccessFile(new File(diskPath), "rw")) {
+
+				
+				randomAccessFile.seek(randomAccessFile.length());
+
+				// obtain file channel from the RandomAccessFile
+				try (FileChannel fileChannel = randomAccessFile.getChannel()) {
+
+					ByteBuffer buf = ByteBuffer.allocate(1000);
+					buf.clear();
+					buf.putInt(i);
+
+					buf.flip();
+
+					while (buf.hasRemaining()) {
+						fileChannel.write(buf);
+					}
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		
+		
+		return false;
+	}
 }
---
+++ src/vfsCore/MainTests.java
@@ -0,0 +1,11 @@
+package vfsCore;
+
+public class MainTests {
+
+	public static void main(String[] args) {
+		Core c = new Core();
+		c.createDisk("test.dsk", 23000);
+
+	}
+
+}
