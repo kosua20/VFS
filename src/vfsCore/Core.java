1e51ab6da8431d79e372b884e42d573b44723183
Simon Rodriguez <kosua20@gmail.com>
2014-12-04 18:51:48 +0100

    Javadoc completed for the import/export classes Comments added Taking into account the new "parent" atttribute of Hierarchy

--- src/vfsCore/Core.java
+++ src/vfsCore/Core.java
@@ -80,6 +80,12 @@ public class Core {
 		}	
 	}
 
+	/**
+	 * imports an element from the host file system. For now, this method copy an element from the project directory to the root of the VFS
+	 * @param homePath the name of the element on the host disk
+	 * @param VFSPath the name of the element on the vfs
+	 * @return  true if the operation is successful
+	 */
 	public boolean importElement(String homePath, String VFSPath) {
 		File fileToAdd = new File(homePath);
 		//Check if the file exists on the host
@@ -91,6 +97,7 @@ public class Core {
 					vfsCore.File hFile = importFile(fileToAdd, VFSPath);
 					//For now, we add it to the root
 					fullHierarchy.addChild(hFile);
+					//And we save the modified Hierarchy
 					cio.saveHierarchyToFile(fullHierarchy);
 					return true;
 				} catch (IOException | CoreIOException | fileNotFound e) {
@@ -104,6 +111,7 @@ public class Core {
 					vfsCore.Folder hFolder = importFolder(fileToAdd);
 					//For now, we add them to the root
 					fullHierarchy.addChild(hFolder);
+					//And we save the modified Hierarchy
 					cio.saveHierarchyToFile(fullHierarchy);
 					return true;
 				} catch (IOException | CoreIOException | fileNotFound e) {
@@ -117,16 +125,34 @@ public class Core {
 		}
 	}
 	
+	/**
+	 * imports a file(java.io.File) to the root of the VFS disk, and give it the specfied name
+	 * @param fileToAdd the File to import
+	 * @param name the name of this file on the VFS
+	 * @return the VFS File corresponding to the imported file
+	 * @throws IOException
+	 * @throws CoreIOException
+	 */
 	public vfsCore.File importFile(File fileToAdd, String name) throws IOException, CoreIOException{
 		//Here check against the remaining space fileToAdd.length();
+		//We write it to the VFS disk
 		long address = cio.writeToDisk(fileToAdd);
-		vfsCore.File hFile = new vfsCore.File(name, address, fileToAdd.length());
+		//We create a new File (subclass of Hierarchy) element
+		vfsCore.File hFile = new vfsCore.File(name, address, fileToAdd.length(), fullHierarchy);
 		return hFile;
 	}
 	
+	/**
+	 * imports a folder, and its files and subfolders to the root of the VFS disk, preserving the hierarchy
+	 * @param folderToAdd a java.io.File element corresponding to the folder
+	 * @return a Folder (subclass of Hierarchy) corresponding to the imported folder
+	 * @throws IOException
+	 * @throws CoreIOException
+	 */
 	public Folder importFolder(File folderToAdd) throws IOException, CoreIOException{
-		Folder folder1 = new Folder(new ArrayList<Hierarchy>(), folderToAdd.getName());
+		Folder folder1 = new Folder(new ArrayList<Hierarchy>(), folderToAdd.getName(), fullHierarchy);
 		//Here check sizeOfFolder(folderToAdd) against available space
+		//We recursively browse the directory and its files & sub-directories
 		for(File file1:folderToAdd.listFiles()){
 			if (file1.isFile()){
 				folder1.addChild(importFile(file1, file1.getName()));
@@ -134,12 +160,18 @@ public class Core {
 				folder1.addChild(importFolder(file1));
 			}
 	    }
-		
 		return folder1;
 	}
 	
+	/**
+	 * exports an element from the VFS .dsk to the project directory (for now) on the host file system 
+	 * @param VFSPath the path of the element in the VFS hierarchy
+	 * @param homePath the name of the element once exported to the host disk
+	 * @return true if the operation is successful
+	 */
 	public boolean exportElement(String VFSPath, String homePath){
 		try {
+			//We find the pointer to the Hierarchy element at the given VFSPath
 			Hierarchy origin = ((Folder)fullHierarchy).findChild(VFSPath);
 			if (origin instanceof vfsCore.File){
 				exportFile((vfsCore.File)origin, homePath);
@@ -152,9 +184,16 @@ public class Core {
 		return false;	
 	}
 	
+	/**
+	 * exports a File (vfsCore.File) to the host system, with the name destination (in the next version, this will allow to specify any path)
+	 * @param file the file to export
+	 * @param destination the name of the file once exported
+	 * @return true if the operation is successful
+	 */
 	public boolean exportFile(vfsCore.File file, String destination){
 		try {
-			System.out.println("exporting file" + file.getName());
+			System.out.println("Exporting file" + file.getName());
+			//We call readFromAdress, with the size of the file
 			return cio.readFromAdress(file.getAddress(), destination, file.getSize());
 		} catch (fileNotFound e) {
 			System.out.println("Fichier non trouvé");
@@ -165,6 +204,11 @@ public class Core {
 		}
 	}
 	
+	/**
+	 * exports a folder from the VFS disk, and its files and subfolders, on the host file system. The hierarchy is conserved
+	 * @param folder the hierarchy Folder to export
+	 * @param destination the name on the folder in the host file system
+	 */
 	public void exportFolder(Folder folder, String destination){
 		//Creating the folder
 		System.out.println("exporting folder" + folder.getName());
@@ -173,16 +217,17 @@ public class Core {
 		//Importing subdirectories and files
 		for(Hierarchy file1:folder.getChildrens()){
 			if (file1 instanceof vfsCore.File){
+				//Exporting the file to the correct subdirectory
 				exportFile((vfsCore.File)file1, destination+File.separator+file1.getName());
 			} else {
+				//Exporting the directory to the correct subdirectory
 				exportFolder((Folder)file1, destination+File.separator+file1.getName());
 			}
 	    }
-		
 	}
 	
 	/**
-	 * Returns the size of a folder, recursively browsing its sub-folders and files
+	 * Returns the size of a folder, recursively browsing its sub-folders and files, on the host file system
 	 * @param folder the folder we want to know the size of
 	 * @return the size of the folder
 	 */
