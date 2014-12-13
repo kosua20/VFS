6f81c64e5700fd111abf4f74b15f78ff7c742dd0
Simon Rodriguez <kosua20@gmail.com>
2014-12-13 13:29:21 +0100

    Hierarchy Test class added.

---
+++ src/vfsCore/HierarchyTest.java
@@ -0,0 +1,49 @@
+package vfsCore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HierarchyTest {
+
+	@Test
+	public void testAddChild() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveChild() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindChild() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlreadyExist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateFolderAtPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenameFolderAtPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateFileAtPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenameFileAtPath() {
+		fail("Not yet implemented");
+	}
+
+}
